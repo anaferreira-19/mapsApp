bfc63c2d323fb68522fd427c836626ab354ab9ab
Ana Ferreira <anafersan60@gmail.com>
2020-12-04 20:49:21 -0300

    Atividade com Google Maps
    Elaboração da atividade que utiliza google maps.

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 
 @NgModule({
@@ -12,8 +13,12 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  providers: [
+    Geolocation
+  ]
 })
-export class HomePageModule {}
+export class HomePageModule { }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,49 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  map: google.maps.Map;
+  minhaPosicao: google.maps.LatLng;
 
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+  constructor(private geolocation: Geolocation) { }
+
+  ionViewWillEnter() {
+    this.exibirMapa();
+  }
+
+  exibirMapa() {
+    const posicao = new google.maps.LatLng(-22.59214676497365, -48.796456755070515);
+    const opcoes = {
+      center: posicao,
+      zoom: 1,
+      disableDafaultUi: true
+    };
+
+    this.map = new google.maps.Map(this.mapRef.nativeElement, opcoes);
+
+    this.buscarPosicao();
+  }
+
+  buscarPosicao() {
+    this.geolocation.getCurrentPosition().then((resp) => {
+      this.minhaPosicao = new google.maps.LatLng(resp.coords.latitude, resp.coords.longitude);
+
+      this.irParaPosicao();
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+  }
+
+  irParaPosicao(){
+    this.map.setCenter(this.minhaPosicao);
+    this.map.setZoom(15);
+
+    const maker = new google.maps.Marker({
+      position: this.minhaPosicao,
+      title: 'Minha Posição',
+      animation: google.maps.Animation.BOUNCE,
+      map: this.map
+    });
+  }
 }
+
